8b89cee427d17a4da974a4255733e282d569188c
BrinkerVII <brinkervii@gmail.com>
2018-05-06 00:41:22 +0200

    Make render scripts a tad clearer

--- build-scripts/build-complete.sh
+++ build-scripts/build-complete.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 
+# Define a bunch of variables
 REPOSITORY_RELATIVE=$(pwd)/..
 REPOSITORY=$(realpath "$REPOSITORY_RELATIVE")
 DO_RENDER_ASSETS=$(realpath ./do-render-assets.sh)
@@ -9,26 +10,38 @@ DIST=$REPOSITORY/dist
 NODE_MODULES=$REPOSITORY/node_modules
 ARC_THEME=$DIST/share/themes/Arc
 
+# We have to build in the repository root
 cd $REPOSITORY
 
+# If the project is already configured, clean it
 if [ -f $CONFIGURE ]; then
+	echo ">> DISTCLEAN"
 	make distclean
 fi
 
 if [ -d "$DIST" ]; then
+	echo ">> REMOVE DIST FILES"
 	rm -rf $DIST/*
 else
+	echo ">> MAKE DIST DIR"
 	mkdir "$DIST"
 fi
 
+echo ">> AUTOGEN"
 $AUTOGEN
+echo ">> CONFIGURE"
 $CONFIGURE --prefix="$DIST"
 
 if [ ! -d "$NODE_MODULES" ]; then
+	echo ">> INSTALLING NODE MODULES"
 	yarn install
+else
+	echo ">> HAVE NODE MODULES"
 fi
 
+echo ">> MAKE CSS"
 yarn run make-css
+echo ">> RENDER ASSETS"
 find -name render-assets.sh -exec "$DO_RENDER_ASSETS" {} \;
 
 make && make all
--- build-scripts/do-render-assets.sh
+++ build-scripts/do-render-assets.sh
@@ -6,23 +6,26 @@ SCRIPT_DIR=$(dirname "$RENDER_SCRIPT")
 ASSETS=$SCRIPT_DIR/assets
 ASSETS_DARK=$SCRIPT_DIR/assets-dark
 
-echo RENDER IT BBY
+echo ">>> RENDERING $SCRIPT_DIR"
 echo $RENDER_SCRIPT
 echo $SCRIPT_DIR
 echo $ASSETS
 echo $ASSETS_DARK
 
+echo ">> CLEAN ASSETS"
 if [ -d "$ASSETS" ]; then
 	rm -rf $ASSETS/*
 else
 	mkdir "$ASSETS"
 fi
 
+echo ">> CLEAN DARK ASSETS"
 if [ -d "$ASSETS_DARK" ]; then
 	rm -rf $ASSETS_DARK/*
 else
 	mkdir "$ASSETS_DARK"
 fi
 
+echo ">> DO RENDER"
 cd $SCRIPT_DIR
 $RENDER_SCRIPT
